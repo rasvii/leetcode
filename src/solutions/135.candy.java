6af9b6e1fb7865bd929c1b409b3029bf85905816
rasvii <rasvi24@gmail.com>
2025-02-06 02:57:23 +0530

    lc 135

---
+++ src/solutions/135.candy.java
@@ -0,0 +1,40 @@
+package solutions;
+/*
+ * @lc app=leetcode id=135 lang=java
+ *
+ * [135] Candy
+ */
+
+// @lc code=start
+class Solution {
+    public int candy(int[] ratings) {
+
+        int[] candies = new int[ratings.length];
+
+        for (int i = 0; i < ratings.length; i++) {
+            candies[i] = 1;
+        }
+
+        for (int i = 1; i < ratings.length; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+
+        for (int i = ratings.length - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i < candies.length; i++) {
+            sum += candies[i];
+        }
+
+        return sum;
+
+    }
+}
+// @lc code=end
