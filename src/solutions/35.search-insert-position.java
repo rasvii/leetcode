374dbb03ea1f8c296d5a3fa0ece71516ab3661f7
rasvii <rasvi24@gmail.com>
2025-04-13 04:22:41 +0530

    lc 35

---
+++ src/solutions/35.search-insert-position.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=35 lang=java
+ *
+ * [35] Search Insert Position
+ */
+
+// @lc code=start
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if (nums[0] == target || nums[0] > target) {
+            return 0;
+        }
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == target || nums[i] > target) {
+                return i;
+            }
+        }
+        return nums.length;
+    }
+}
+// @lc code=end
