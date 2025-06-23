02f55cbc077891cff2bc17f0addaba7684630b26
rasvii <rasvi24@gmail.com>
2025-06-24 03:19:00 +0530

    lc 704

---
+++ src/solutions/704.binary-search.java
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=704 lang=java
+ *
+ * [704] Binary Search
+ */
+
+// @lc code=start
+class Solution {
+    public int search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        
+        while(low <= high){
+            int mid = low +  (high - low)/2;
+            
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(target < nums[mid]){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        
+        return -1;
+    }
+}
+// @lc code=end
+
