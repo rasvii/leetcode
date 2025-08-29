069a89577a896d8689d0ea439433d746cdb472a2
rasvii <rasvi24@gmail.com>
2025-08-30 03:40:42 +0530

    lc 191 347

--- src/solutions/347.top-k-frequent-elements.java
+++ src/solutions/347.top-k-frequent-elements.java
@@ -7,33 +7,63 @@ package solutions;
 
 // @lc code=start
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.PriorityQueue;
+import java.util.*;
 
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
 
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        for (int n : nums) {
+            freq.put(n, freq.getOrDefault(n, 0) + 1);
         }
 
-        PriorityQueue<Map.Entry<Integer, Integer>> heap = new PriorityQueue<>(
-                (a, b) -> Integer.compare(b.getValue(), a.getValue()));
-
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            heap.add(entry);
+        List<Integer>[] bucket = new List[nums.length + 1];
+        for (int n : freq.keySet()) {
+            int val = freq.get(n);
+            if (bucket[val] == null) {
+                bucket[val] = new ArrayList<>();
+            }
+            bucket[val].add(n);
         }
 
-        int[] result = new int[k];
-        for (int i = 0; i < k; i++) {
-            result[i] = heap.poll().getKey();
+        List<Integer> result = new ArrayList<>();
+        for (int i = bucket.length - 1; i >= 0; i--) {
+            if (bucket[i] == null)
+                continue;
+            result.addAll(bucket[i]);
+            if (result.size() >= k) {
+                break;
+            }
         }
 
-        return result;
-
+        return result.stream().mapToInt(Integer::valueOf).toArray();
     }
 }
+/*
+ * class Solution {
+ * public int[] topKFrequent(int[] nums, int k) {
+ * 
+ * HashMap<Integer, Integer> map = new HashMap<>();
+ * 
+ * for (int num : nums) {
+ * map.put(num, map.getOrDefault(num, 0) + 1);
+ * }
+ * 
+ * PriorityQueue<Map.Entry<Integer, Integer>> heap = new PriorityQueue<>(
+ * (a, b) -> Integer.compare(b.getValue(), a.getValue()));
+ * 
+ * for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+ * heap.add(entry);
+ * }
+ * 
+ * int[] result = new int[k];
+ * for (int i = 0; i < k; i++) {
+ * result[i] = heap.poll().getKey();
+ * }
+ * 
+ * return result;
+ * 
+ * }
+ * }
+ */
 // @lc code=end
