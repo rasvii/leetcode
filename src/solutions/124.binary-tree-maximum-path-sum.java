639e475eec9937ed2eb3b4af3f6276d446c2dc5d
rasvii <rasvi24@gmail.com>
2025-09-01 02:21:37 +0530

    lc 124 1143

---
+++ src/solutions/124.binary-tree-maximum-path-sum.java
@@ -0,0 +1,44 @@
+package solutions;
+/*
+ * @lc app=leetcode id=124 lang=java
+ *
+ * [124] Binary Tree Maximum Path Sum
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    int max = Integer.MIN_VALUE;
+
+    public int maxPathSum(TreeNode root) {
+        helper(root);
+        return max;
+    }
+
+    private int helper(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        int left = Math.max(helper(root.left), 0);
+        int right = Math.max(helper(root.right), 0);
+
+        max = Math.max(max, left + right + root.val);
+
+        return root.val + Math.max(left, right);
+    }
+}
+// @lc code=end
