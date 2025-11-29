c77d0e92cd37a2ba67eb21dbdb7a90b3dfa8bfc2
rasvii <rasvi24@gmail.com>
2025-11-30 00:59:26 +0530

    lc 3512

---
+++ src/solutions/3512.java
@@ -0,0 +1,15 @@
+package solutions;
+
+class Solution {
+    public int minOperations(int[] nums, int k) {
+
+        int sum = 0;
+
+        for (int n : nums) {
+            sum += n;
+        }
+
+        return sum % k;
+
+    }
+}
