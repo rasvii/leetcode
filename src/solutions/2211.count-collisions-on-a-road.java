9666a14d539829390173c6d875a12e297805007e
rasvii <rasvi24@gmail.com>
2025-12-05 01:37:19 +0530

    lc 2211

---
+++ src/solutions/2211.count-collisions-on-a-road.java
@@ -0,0 +1,52 @@
+package solutions;
+/*
+ * @lc app=leetcode id=2211 lang=java
+ *
+ * [2211] Count Collisions on a Road
+ */
+
+// @lc code=start
+
+import java.util.Stack;
+
+class Solution {
+    public int countCollisions(String directions) {
+
+        Stack<Character> stack = new Stack<>();
+        int result = 0;
+        int n = directions.length();
+        // stack.push(directions.charAt(0));
+
+        for (int i = 0; i < n; i++) {
+            char c = directions.charAt(i);
+
+            if (stack.isEmpty()) {
+                stack.push(c);
+                continue;
+            }
+
+            char peek = stack.peek();
+
+            if (peek == 'R' && c == 'L') {
+                result += 2;
+                stack.pop();
+                c = 'S';
+
+            } else if (peek == 'S' && c == 'L') {
+                result++;
+                c = 'S';
+
+            }
+
+            while (!stack.isEmpty() && (stack.peek() == 'R' && c == 'S')) {
+                result++;
+                stack.pop();
+            }
+
+            stack.push(c);
+        }
+
+        return result;
+    }
+}
+// @lc code=end
