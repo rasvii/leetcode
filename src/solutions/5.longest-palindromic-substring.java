5e9c9a29fd91736de8d1bee9ded7a67f9e009491
rasvii <rasvi24@gmail.com>
2025-12-09 01:01:17 +0530

    lc 5 optimized

--- src/solutions/5.longest-palindromic-substring.java
+++ src/solutions/5.longest-palindromic-substring.java
@@ -6,33 +6,78 @@ package solutions;
  */
 
 // @lc code=start
+
 class Solution {
     public String longestPalindrome(String s) {
 
-        if (s.length() <= 1) {
-            return s;
+        int maxLen = 0, left = 0, right = 0, n = s.length();
+        int[] vals = new int[2];
+
+        for (int i = 0; i < n; i++) {
+
+            helper(s, i, i, vals);
+
+            if (vals[1] - vals[0] > maxLen) {
+                left = vals[0];
+                right = vals[1];
+                maxLen = vals[1] - vals[0];
+            }
+
+            helper(s, i, i + 1, vals);
+
+            if (vals[1] - vals[0] > maxLen) {
+                left = vals[0];
+                right = vals[1];
+                maxLen = vals[1] - vals[0];
+            }
         }
 
-        String longestPalindrome = s.substring(0, 1);
+        return s.substring(left, right);
 
-        for (int i = 0; i < s.length(); i++) {
-            String length = isPalindrome(s, i, i + 1);
-            longestPalindrome = length.length() > longestPalindrome.length() ? length : longestPalindrome;
-            length = isPalindrome(s, i - 1, i + 1);
-            longestPalindrome = length.length() > longestPalindrome.length() ? length : longestPalindrome;
-        }
-
-        return longestPalindrome;
     }
 
-    private String isPalindrome(String s, int left, int right) {
-
+    private void helper(String s, int left, int right, int[] vals) {
         while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
             left--;
             right++;
         }
 
-        return s.substring(left + 1, right);
+        vals[0] = left + 1;
+        vals[1] = right;
     }
 }
+/*
+ * class Solution {
+ * public String longestPalindrome(String s) {
+ * 
+ * if (s.length() <= 1) {
+ * return s;
+ * }
+ * 
+ * String longestPalindrome = s.substring(0, 1);
+ * 
+ * for (int i = 0; i < s.length(); i++) {
+ * String length = isPalindrome(s, i, i + 1);
+ * longestPalindrome = length.length() > longestPalindrome.length() ? length :
+ * longestPalindrome;
+ * length = isPalindrome(s, i - 1, i + 1);
+ * longestPalindrome = length.length() > longestPalindrome.length() ? length :
+ * longestPalindrome;
+ * }
+ * 
+ * return longestPalindrome;
+ * }
+ * 
+ * private String isPalindrome(String s, int left, int right) {
+ * 
+ * while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right))
+ * {
+ * left--;
+ * right++;
+ * }
+ * 
+ * return s.substring(left + 1, right);
+ * }
+ * }
+ */
 // @lc code=end
