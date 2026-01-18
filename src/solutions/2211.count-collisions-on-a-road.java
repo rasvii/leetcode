f248cdd680fdf3ab2d1c1e4dd8a3f4b1e28320ed
Rasvi Jambhulkar <rasvi24@gmail.com>
2026-01-19 01:20:38 +0530

    lc 2211

--- src/solutions/2211.count-collisions-on-a-road.java
+++ src/solutions/2211.count-collisions-on-a-road.java
@@ -11,42 +11,69 @@ import java.util.Stack;
 
 class Solution {
     public int countCollisions(String directions) {
-
-        Stack<Character> stack = new Stack<>();
-        int result = 0;
-        int n = directions.length();
-        // stack.push(directions.charAt(0));
-
-        for (int i = 0; i < n; i++) {
-            char c = directions.charAt(i);
-
-            if (stack.isEmpty()) {
-                stack.push(c);
-                continue;
+        int res = 0;
+        int flag = -1;
+        for (char c : directions.toCharArray()) {
+            if (c == 'L') {
+                if (flag >= 0) {
+                    res += flag + 1;
+                    flag = 0;
+                }
+            } else if (c == 'S') {
+                if (flag > 0) {
+                    res += flag;
+                }
+                flag = 0;
+            } else {
+                if (flag >= 0) {
+                    flag++;
+                } else {
+                    flag = 1;
+                }
             }
-
-            char peek = stack.peek();
-
-            if (peek == 'R' && c == 'L') {
-                result += 2;
-                stack.pop();
-                c = 'S';
-
-            } else if (peek == 'S' && c == 'L') {
-                result++;
-                c = 'S';
-
-            }
-
-            while (!stack.isEmpty() && (stack.peek() == 'R' && c == 'S')) {
-                result++;
-                stack.pop();
-            }
-
-            stack.push(c);
         }
-
-        return result;
+        return res;
     }
 }
+
+// class Solution {
+//     public int countCollisions(String directions) {
+
+//         Stack<Character> stack = new Stack<>();
+//         int result = 0;
+//         int n = directions.length();
+//         // stack.push(directions.charAt(0));
+
+//         for (int i = 0; i < n; i++) {
+//             char c = directions.charAt(i);
+
+//             if (stack.isEmpty()) {
+//                 stack.push(c);
+//                 continue;
+//             }
+
+//             char peek = stack.peek();
+
+//             if (peek == 'R' && c == 'L') {
+//                 result += 2;
+//                 stack.pop();
+//                 c = 'S';
+
+//             } else if (peek == 'S' && c == 'L') {
+//                 result++;
+//                 c = 'S';
+
+//             }
+
+//             while (!stack.isEmpty() && (stack.peek() == 'R' && c == 'S')) {
+//                 result++;
+//                 stack.pop();
+//             }
+
+//             stack.push(c);
+//         }
+
+//         return result;
+//     }
+// }
 // @lc code=end
