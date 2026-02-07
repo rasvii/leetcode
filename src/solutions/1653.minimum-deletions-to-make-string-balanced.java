8767d18e465d8d9fda0cbdf15b33026770fd32c1
Rasvi Jambhulkar <rasvi24@gmail.com>
2026-02-08 01:41:06 +0530

    lc 1653

---
+++ src/solutions/1653.minimum-deletions-to-make-string-balanced.java
@@ -0,0 +1,35 @@
+package solutions;
+/*
+ * @lc app=leetcode id=1653 lang=java
+ *
+ * [1653] Minimum Deletions to Make String Balanced
+ */
+
+// @lc code=start
+class Solution {
+    public int minimumDeletions(String s) {
+
+        int n = s.length();
+        int totalA = 0;
+
+        for(char c: s.toCharArray()) {
+            if(c == 'a') totalA++;
+        }
+
+        int bCount = 0;
+        int result = n;
+
+        int a = 0, b = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(s.charAt(i) == 'a') totalA--;
+            result = Math.min(result, totalA + bCount);
+            if(s.charAt(i) == 'b') bCount++;
+        }
+
+        return result;
+        
+    }
+}
+// @lc code=end
+
