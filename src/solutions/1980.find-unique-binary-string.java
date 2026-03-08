da572bb90be72a4927eb67eeee4a68f24a2bf662
Rasvi Jambhulkar <rasvi24@gmail.com>
2026-03-09 01:01:40 +0530

    lc 1980

--- src/solutions/1980.find-unique-binary-string.java
+++ src/solutions/1980.find-unique-binary-string.java
@@ -37,5 +37,46 @@ class Solution {
         }
     }
 }
+/* 
+class Solution {
+    public String findDifferentBinaryString(String[] nums) {
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0;i<nums.length;i++)
+        {
+            sb.append(nums[i].charAt(i) == '0'? "1": "0");
+        }
+        return new String(sb);
+    }
+}
+
+
+class Solution {
+    public String findDifferentBinaryString(String[] nums) {
+        
+        String[] bi = {"0", "1"};
+        Set<String> numsSet = new HashSet<>(Arrays.asList(nums));
+
+        return backtrack(0, bi, nums.length, new StringBuilder(), numsSet);
+    }
+
+    private String backtrack(int idx, String[] bi, int n, StringBuilder sb, Set<String> numsSet) {
+
+        if(sb.length() == n) {
+            String result = sb.toString();
+            if(!numsSet.contains(result)) return result;
+            return null;
+        }
+
+        for(int i = idx; i < 2; i++) {
+            sb.append(bi[i]);
+            String result = backtrack(i, bi, n, sb, numsSet);
+            if(result != null) return result;
+            sb.deleteCharAt(sb.length() - 1);
+        }
+
+        return null;
+    }
+} */
 // @lc code=end
 
