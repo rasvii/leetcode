a87bd06809b42e9fc99d2ba9705e79add7e8cccd
Rasvi Jambhulkar <rasvi24@gmail.com>
2026-06-19 02:21:08 +0530

    lc 1344

---
+++ src/solutions/1344.angle-between-hands-of-a-clock.java
@@ -0,0 +1,17 @@
+package solutions;
+/*
+ * @lc app=leetcode id=1344 lang=java
+ *
+ * [1344] Angle Between Hands of a Clock
+ */
+
+// @lc code=start
+class Solution {
+    public double angleClock(int hour, int minutes) {
+        double x = hour + minutes / 60.0;
+        double diff = (11.0 * x) % 12.0;
+        return Math.min(diff, 12.0 - diff) * 30.0;
+    }
+}
+// @lc code=end
+
